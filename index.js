e510bda0ec36355115d1e91fa4e5e55dc972f224
Jirat Onaree <jirat.onaree@gmail.com>
2018-01-01 14:40:15 +0700

    Init project with Micro :tada: (no linter)

---
+++ index.js
@@ -0,0 +1,11 @@
+const { send } = require('micro');
+const { router, get } = require('microrouter');
+
+const hello = () => 'hello GIFE!!';
+
+const notFound = (req, res) => send(res, 404, 'Not Found !! use /hello');
+
+module.exports = router(
+  get('/hello', hello),
+  get('/*', notFound),
+);
