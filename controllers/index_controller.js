4732f523589927d70725e0f791b70dc5d70359ec
Dreamiiz <dream10005@gmail.com>
2018-01-03 03:35:42 +0700

    show all places from mysql success!! :tada:

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -1,26 +1,26 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool  = mysql.createPool({
   host     : '103.70.5.81',
   user     : 'gifebeta_root',
   password : 'password',
   database : 'gifebeta_db',
 });
 
-connection.connect(function(err) {
-	if (err) {
-		console.log(err);
-	}
-	connection.query("SELECT place_name FROM Places WHERE place_id = 1", function (err, result){
-		if (err) {
-			console.log(err);
-		}
-		console.log(result);
-	});
-});
+let a = [];
 
 module.exports	= {
 	hello: function() {
-		return "Hello";
+		pool.getConnection(function(err, connection) {
+		    connection.query("SELECT * FROM Places", function(err, rows) {
+		    	console.log(rows[1]);
+		    	for (var i = 0; i <= rows.length - 1; i++) {
+		    		a[i] = rows[i];
+		    		console.log(a[i]);
+		    	}
+		        connection.release();
+		    });
+		});
+		return a[1];
 	}
 };
