0d9e34b3e605c701ba74de06072d6a57a0a1d32e
Jirat Onaree <jirat.onaree@gmail.com>
2018-01-11 18:05:17 +0700

    Refactoring main index

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+// Micro
 const { send } = require('micro');
 const { router, get } = require('microrouter');
-const indexcontroller = require('./controllers/index_controller.js');
 
-const notFound = (req, res) => send(res, 404, 'Not Found !! use /hello');
+// Services (of course)
+const User = require('./');
+const Place = require('./services/Place');
+const Challenge = require('./services/Challenge');
+const Gife = require('./services/Gife');
+const Reward = require('./services/Reward');
+
+const notFound = (req, res) => send(res, 404, 'Hi Hacker!');
 
 module.exports = router(
-  get('/hello', indexcontroller.getAllPlaces),
+  // Ping Ping
+  get('/ping/user', User.ping),
+  get('/ping/place', Place.ping),
+  get('/ping/challenge', Challenge.ping),
+  get('/ping/gife', Gife.ping),
+  get('/ping/reward', Reward.ping),
+  // Welcoming hacker zone
   get('/*', notFound),
 );
